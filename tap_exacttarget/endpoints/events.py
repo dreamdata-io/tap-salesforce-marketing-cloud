f816f5a993971e9be2856608e7f7445251d0a5dd
JingLin0 <jinglin.daisy@gmail.com>
2024-09-24 16:42:50 +0200

    fix bookmark key

--- tap_exacttarget/endpoints/events.py
+++ tap_exacttarget/endpoints/events.py
@@ -5,7 +5,11 @@ import singer
 from tap_exacttarget.client import request
 from tap_exacttarget.dao import DataAccessObject, exacttarget_error_handling
 from tap_exacttarget.pagination import get_date_page, before_now, increment_date
-from tap_exacttarget.state import incorporate, save_state, get_last_record_value_for_table
+from tap_exacttarget.state import (
+    incorporate,
+    save_state,
+    get_last_record_value_for_table,
+)
 
 
 LOGGER = singer.get_logger()
@@ -79,7 +83,7 @@ class EventDataAccessObject(DataAccessObject):
                         )
                         continue
 
-                    self.write_records_with_transform(event, catalog_copy, table)
+                    self.write_records_with_transform(event, catalog_copy, event_name)
 
                 self.state = incorporate(self.state, event_name, "EventDate", start)
 
